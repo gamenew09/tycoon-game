898635003bfd90ec093a7b1a6be0fbff39dbda56
gamenew09 <gamenew09@gmail.com>
2021-11-04 05:07:17 -0500

    Some documenting

--- src/shared/sounds.ts
+++ src/shared/sounds.ts
@@ -1,5 +1,11 @@
 export const SoundDatas = {
+    /**
+     * This sound is played when the player buys an item for their Tycoon.
+     */
     bought_item: "rbxassetid://131886985",
+    /**
+     * This sound is played when the player collects cash from their Tycoon's bank.
+     */
     cash_from_bank: ["rbxassetid://152206246", "rbxassetid://152206206"],
 };
 export type SoundDatas = typeof SoundDatas;
