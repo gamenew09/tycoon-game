bdd8a533b75695844972f91cab447ffe360ac1b5
gamenew09 <gamenew09@gmail.com>
2021-11-05 06:31:16 -0500

    Documented & Fixed ContentProviderAsync Didn't realize that the callback was called for each asset, so now we check and make sure all assets are preloaded before we resolve and we reject if any fail. Timeouts on these assets can be done using Promise.race and Promise.delay().then(Promise.reject) pattern.

--- src/client/ContentProviderAsync.ts
+++ src/client/ContentProviderAsync.ts
@@ -1,9 +1,28 @@
 import { ContentProvider } from "@rbxts/services";
 
+/**
+ * A utility namespace that makes some of the ContentProvider functions use Promises instead of callbacks.
+ */
 namespace ContentProviderAsync {
-    export function Preload(contentInstances: Instance[]): Promise<[contentId: string, status: Enum.AssetFetchStatus]> {
-        return new Promise((resolve) => {
-            ContentProvider.PreloadAsync(contentInstances, (contentId, status) => resolve([contentId, status]));
+    /**
+     * Promises to preload ALL the given assets using `ContentProvider.PreloadAsync`
+     * @param contentInstances The instances to preload.
+     * @returns A promise to preload all assets.
+     */
+    export function Preload(contentInstances: Instance[]): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const instancesGiven = contentInstances.size(); // We're gonna cache the count of instances given so we don't accidentally hold the instances in limbo forever.
+            const ids: string[] = []; // We're gonna go by count to check if all assets are preloaded for this call.
+            ContentProvider.PreloadAsync(contentInstances, (contentId, status) => {
+                if (status === Enum.AssetFetchStatus.Success) {
+                    ids.push(contentId);
+                    if (ids.size() >= instancesGiven) {
+                        resolve();
+                    }
+                } else {
+                    reject(`${contentId} failed to preload.`);
+                }
+            });
         });
     }
 }
