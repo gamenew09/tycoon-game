a4dd32ce38808668239dcc24a95e2cf86cc09add
gamenew09 <gamenew09@gmail.com>
2021-11-04 21:45:43 -0500

    Added a Loading UI Should be useful when we end up properly adding asset loading later.

--- src/client/controllers/UIManagement.tsx
+++ src/client/controllers/UIManagement.tsx
@@ -6,12 +6,17 @@ import MainUI from "client/ui/components/MainUI";
 import { assertLog } from "shared/logutil";
 import { ClientLog } from "./ClientLog";
 import { Logger } from "@rbxts/log";
+import LoadingUI from "client/ui/components/LoadingUI";
+import tutil from "shared/tutil";
 
 @Controller({
     loadOrder: -99,
 })
 export class UIManagement implements OnStart, OnInit {
     private mainTree!: Roact.Tree;
+    private loadTree: Roact.Tree | undefined;
+
+    private PlayerGui!: PlayerGui;
 
     constructor(private clientLog: ClientLog) {}
 
@@ -28,11 +33,23 @@ export class UIManagement implements OnStart, OnInit {
 
     onInit() {
         this._log = this.clientLog.forController(UIManagement);
+        const playerGui = Players.LocalPlayer!.WaitForChild("PlayerGui");
+        this.assert(tutil.InstanceIsA.PlayerGui(playerGui), "PlayerGui is not a Class of PlayerGui, whaaaa");
+        this.PlayerGui = playerGui;
 
-        // TODO: Implement loading UI here, until we start all controllers.
+        this.getLog().Info("Mounting LoadingUI.");
+        this.loadTree = Roact.mount(<LoadingUI MaxDots={5} TimeBetweenDots={1}></LoadingUI>, playerGui);
+        this.getLog().Info("Mounted LoadingUI.");
     }
 
     onStart() {
+        this.getLog().Info("Unmounting LoadingUI.");
+        if (this.loadTree !== undefined) {
+            Roact.unmount(this.loadTree);
+        }
+        this.loadTree = undefined;
+        this.getLog().Info("Unmounted LoadingUI.");
+
         this.getLog().Info("Mounting MainUI.");
         this.mainTree = Roact.mount(
             <GameStateProvider>
@@ -40,7 +57,7 @@ export class UIManagement implements OnStart, OnInit {
                     <MainUI />
                 </screengui>
             </GameStateProvider>,
-            Players.LocalPlayer!.WaitForChild("PlayerGui"),
+            this.PlayerGui,
             "MainTree",
         );
         this.getLog().Info("Mounted MainUI.");
---
+++ src/client/ui/components/LoadingUI.tsx
@@ -0,0 +1,69 @@
+import Maid from "@rbxts/maid";
+import Roact from "@rbxts/roact";
+import { RunService } from "@rbxts/services";
+
+interface LoadingUIState {
+    dots: number;
+    nextDot: number;
+}
+
+interface LoadingUIProps {
+    TimeBetweenDots: number;
+    MaxDots: number;
+}
+
+export default class LoadingUI extends Roact.Component<LoadingUIProps, LoadingUIState> {
+    private maid: Maid = new Maid();
+
+    constructor(p: LoadingUIProps) {
+        super(p);
+
+        this.state = {
+            dots: 0,
+            nextDot: 0,
+        };
+    }
+
+    didMount(): void {
+        this.setState({
+            nextDot: tick() + this.props.TimeBetweenDots,
+        });
+        this.maid.GiveTask(
+            RunService.Heartbeat.Connect((delta) => {
+                if (tick() >= this.state.nextDot) {
+                    let nextDotCount = this.state.dots + 1;
+                    if (nextDotCount > this.props.MaxDots) {
+                        nextDotCount = 0;
+                    }
+
+                    this.setState({
+                        dots: nextDotCount,
+                        nextDot: tick() + this.props.TimeBetweenDots,
+                    });
+                }
+            }),
+        );
+    }
+
+    willUnmount(): void {
+        this.maid.Destroy();
+    }
+
+    public render(): Roact.Element | undefined {
+        let dots = "";
+        for (let i = 0; i < this.state.dots; i++) {
+            dots += ".";
+        }
+        return (
+            <screengui IgnoreGuiInset={true}>
+                <textlabel
+                    Position={new UDim2(0, 0, 0, 0)}
+                    Size={new UDim2(1, 0, 1, 0)}
+                    BackgroundTransparency={0}
+                    BackgroundColor3={Color3.fromRGB(0, 0, 0)}
+                    Text={`Loading${dots}`}
+                />
+            </screengui>
+        );
+    }
+}
